733de04933585c7991f5848547f4843d05f4c376
Will Vincent <will@wsvincent.com>
2025-03-19 08:26:07 -0400

    better formatted admin

--- predict/admin.py
+++ predict/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
 from .models import IrisPrediction
 
-admin.site.register(IrisPrediction)
+
+@admin.register(IrisPrediction)
+class IrisPredictionAdmin(admin.ModelAdmin):
+    list_display = (
+        "prediction",
+        "created_at_display",
+        "sepal_length",
+        "sepal_width",
+        "petal_length",
+        "petal_width",
+    )
+    list_filter = (
+        "created_at",
+        "prediction",
+    )  # Enables filtering by date and prediction type
+    search_fields = ("prediction",)  # Adds search functionality
+    ordering = ("-created_at",)  # Orders by most recent predictions
+
+    def created_at_display(self, obj):
+        return obj.created_at.strftime("%Y-%m-%d %H:%M")
+
+    created_at_display.admin_order_field = "created_at"
+    created_at_display.short_description = "Created At"
